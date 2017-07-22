5d529f669f0bcd91e193c40ede30746042dff93e
Ian McWilliams <ian.mcwilliams@inps.co.uk>
2017-07-22 10:07:22 +0100

    add ct refund transaction

--- derive_journal/journal_transaction.rb
+++ derive_journal/journal_transaction.rb
@@ -57,6 +57,10 @@ class JournalTransaction
             ct_payable:                     :dr,
             cash:                           :cr
         },
+        corporation_tax_refund: {
+            cash:                           :dr,
+            ct_payable:                     :cr
+        },
         accrue_corporation_tax: {
             capital:                        :dr,
             ct_payable:                     :cr

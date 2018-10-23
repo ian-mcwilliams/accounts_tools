95e1279a8e267986dfa08e92d971d72900fc47c3
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-10-23 21:44:24 +0100

    add accounts ingress tests

---
+++ spec/accounts_ingress_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../tax_management/accounts_ingress'
+require_relative 'spec_helpers'
+require 'rspec'
+
+describe AccountsIngress do
+  include TaxCheckerSpecHelpers
+
+  it 'gets just the first period hash when period 1 specified' do
+    accounts_hash = AccountsIngress.accounts_summaries_ingress(1, true)
+    expect(accounts_hash.keys.sort).to eq(%i[previous_period current_period].sort)
+    expect(accounts_hash[:previous_period]).to be(nil)
+    expect(accounts_hash[:current_period].class).to be(Array)
+    expected = TaxCheckerSpecHelpers.expected_first_period_ingress_hash_array
+    accounts_hash[:current_period].each_with_index do |account_hash, i|
+      expect(account_hash).to eq(expected[i])
+    end
+  end
+
+  it 'gets the first and second period hashes when period 2 specified' do
+    accounts_hash = AccountsIngress.accounts_summaries_ingress(2, true)
+    expect(accounts_hash.keys.sort).to eq(%i[previous_period current_period].sort)
+    expect(accounts_hash[:previous_period].class).to be(Array)
+    expected = TaxCheckerSpecHelpers.expected_first_period_ingress_hash_array
+    accounts_hash[:previous_period].each_with_index do |account_hash, i|
+      expect(account_hash).to eq(expected[i])
+    end
+    expect(accounts_hash[:current_period].class).to be(Array)
+    expected = TaxCheckerSpecHelpers.expected_second_period_ingress_hash_array
+    accounts_hash[:current_period].each_with_index do |account_hash, i|
+      expect(account_hash).to eq(expected[i])
+    end
+  end
+
+end
---
+++ spec/spec_helpers.rb
@@ -0,0 +1,1 @@
+require_relative 'support/tax_checker_spec_helpers'

4c60769929f2d1b978379168c0cbe719a2dc2381
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-10-24 18:30:39 +0100

    amendments to satisfy test requirements

--- tax_management/accounts_ingress.rb
+++ tax_management/accounts_ingress.rb
@@ -1,31 +1,37 @@
+require 'simple_xlsx_reader'
+
 module AccountsIngress
 
-  def self.accounts_summaries_ingress(period)
+  def self.accounts_summaries_ingress(period, test=false)
     unless period.is_a?(Integer) && period > 0
       raise('period must be an integer greater than 0')
     end
     accounts_summaries = {
-      current_period: accounts_summary_array(period),
+      current_period: accounts_summary_array(period, test),
       previous_period: nil
     }
     if period > 1
-      accounts_summaries[:previous_period] = accounts_summary_array(period - 1)
+      accounts_summaries[:previous_period] = accounts_summary_array(period - 1, test)
     end
     accounts_summaries
   end
 
-  def self.accounts_summary_rows(period)
-    file = SimpleXlsxReader.open(accounts_file_path(period))
+  def self.accounts_summary_rows(period, test=false)
+    file = SimpleXlsxReader.open(accounts_file_path(period, test))
     file.sheets.each do |sheet|
       return sheet.rows if sheet.name == 'Accounts Summary'
     end
   end
 
-  def self.accounts_file_path(period)
-    rel_dropbox_path = '../../Dropbox/'
-    livecorp_path = 'F3Mmedia/Internal/ACcounts/LiveCorp/'
-    accounts_file_path = "#{period}.#{period_strings[period - 1]}/Accounts#{period}.xlsx"
-    "#{rel_dropbox_path}#{livecorp_path}#{accounts_file_path}"
+  def self.accounts_file_path(period, test=false)
+    if test
+      "spec/test_artefacts/TestAccounts#{period}.xlsx"
+    else
+      rel_dropbox_path = '../../Dropbox/'
+      livecorp_path = 'F3Mmedia/Internal/ACcounts/LiveCorp/'
+      accounts_file_path = "#{period}.#{period_strings[period - 1]}/Accounts#{period}.xlsx"
+      "#{rel_dropbox_path}#{livecorp_path}#{accounts_file_path}"
+    end
   end
 
   def self.period_strings
@@ -42,11 +48,11 @@ module AccountsIngress
     ]
   end
 
-  def self.accounts_summary_array(period)
-    rows = accounts_summary_rows(period)
+  def self.accounts_summary_array(period, test=false)
+    rows = accounts_summary_rows(period, test)
     (1..27).each_with_object([]) do |i, a|
       account_code = rows[i][1][/^\D\d+/]
-      account_name = rows[i][1].delete("#{account_code}. ")
+      account_name = rows[i][1].gsub("#{account_code}. ", '')
       a << {
         account_code: account_code,
         account_name: account_name,

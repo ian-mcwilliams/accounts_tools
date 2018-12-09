81de83b02f1eb72a3b1d7f6115fcd87f24707575
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-12-09 17:21:53 +0000

    refine test flag for generate_reports_summary, and re-add error message (must have been reverted by accident??)

--- tax_management/tax_checker.rb
+++ tax_management/tax_checker.rb
@@ -5,21 +5,22 @@ require_relative 'ct_return'
 module TaxChecker
 
   def self.generate_ct_return_inputs(period, inputs)
-    summary = generate_reports_summary(period, inputs)
+    summary = generate_reports_summary(period, inputs, false)
     CtReturn.corporation_tax_return_inputs(summary, inputs[:current], inputs[:previous])
   end
 
   def self.generate_ch_accounts(period, inputs)
-    summary = generate_reports_summary(period, inputs)
+    summary = generate_reports_summary(period, inputs, false)
     ChAccounts.abbreviated_accounts(summary)
   end
 
-  def self.generate_reports_summary(period, inputs)
-    accounts_summaries = AccountsIngress.accounts_summaries_ingress(period, true)
+  def self.generate_reports_summary(period, inputs, test = true)
+    accounts_summaries = AccountsIngress.accounts_summaries_ingress(period, test)
     ReportsSummary.reports_summary(accounts_summaries, inputs)
   end
 
-  def self.calculation_inputs(period = nil)
+  def self.calculation_inputs(period)
+    raise("the period must be greater than 0 - got #{period}") unless period > 0
     all_periods = JSON.parse(File.read('tax_management/calculation_inputs.json'))['calculation_inputs']
     inputs_hash = { current: all_periods.find { |item| item['period'] == period } }
     inputs_hash[:previous] = all_periods.find { |item| item['period'] == period - 1 } if period > 1

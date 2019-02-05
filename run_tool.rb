59683b82229dece4a6c9b84f60bb596554b02b38
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2019-02-05 21:49:28 +0000

    set account tool env variable and modularise so that tools can call each other if needed

--- run_tool.rb
+++ run_tool.rb
@@ -1,51 +1,35 @@
 require_relative 'env'
+args = ARGV.map { |arg| arg }
+target = args.shift
 
 # THIS FILE SHOULD EVENTUALLY CONTAIN ALL ACCOUNTS TOOLS CALLS AND INFO ON HOW TO RUN EACH
 
 # The call to this file takes an argument specifying what task to run, see the documentation for the relevant task for
 # details of how to make a valid call
 
-target = nil
-args = []
+module RunTool
 
-ARGV.each_with_index { |arg, index| index == 0 ? target = arg : args << arg }
+  def run_tool(target, args)
+    store_target = ENV['ACCOUNT_TOOL']
+    ENV['ACCOUNT_TOOL'] = target
 
+    case ENV['ACCOUNT_TOOL']
 
+    when 'convert_bank_extract'
+      require_relative 'convert_bank_extract/lib/convert_bank_extract'
+      ConvertBankExtract.convert_bank_extract
 
-# CONVERT BANK STATEMENT
-#
-# This tool is designed to take a downloaded csv from Barclays containing account entries as an input, and it will
-# output a console which can be copied directly to the bank.xlsx bookkeeping file
-#
-#
-#       *** !!!   ALWAYS REMEMBER   !!! ***      *** !!!   ALWAYS REMEMBER   !!! ***
-#
-# first download the bank statement and save it in records/bank_statements
-# the statement filename is based on the OPENING and CLOSING transactions dates on the statement
-# when you download from Barclays the extract MUST BE BASED ON A SEARCH FOR THE SAME TWO DATES
-#
-#       *** !!!   ALWAYS REMEMBER   !!! ***      *** !!!   ALWAYS REMEMBER   !!! ***
-#
-#
-# TO RUN:
-#  - save the target file to the source_file directory as data.csv (this is the default filename but watch out for
-#      duplicate naming avoidance like "data (1).csv")
-#  - ensure the sheet's tab name is "data" (this is the current default so no change should be needed)
-#  - IN TERMINAL: ruby run_tool.rb convert_bank_extract
-#  - find the result in the run_results/convert_bank_extract directory (old results are overwritten)
-#  - copy and paste the row VALUES ONLY into the bank.xlsx spreadsheet (NOTE: the empty column D is there... honest!)
-#  - copy/drag down the balance column and id column and add the statement filename and period id
-#  - verify that the balance on the last row matches the balance on the final transaction on the bank statement
+    when 'reconcile_sales'
+      require_relative 'contract_management/lib/books_ingress'
+      BooksIngress.import_books
 
-if target == 'convert_bank_extract'
-  require_relative 'convert_bank_extract/lib/convert_bank_extract'
-  ConvertBankExtract.convert_bank_extract
+    else
+      raise("no tool found for target: '#{target}'")
+    end
+
+    ENV['ACCOUNT_TOOL'] = store_target if store_target
+  end
+
 end
 
-
-# RECONCILE SALES
-
-if target == 'reconcile_sales'
-  require_relative 'contract_management/lib/books_ingress'
-  BooksIngress.import_books
-end
+RunTool.run_tool(target, args)

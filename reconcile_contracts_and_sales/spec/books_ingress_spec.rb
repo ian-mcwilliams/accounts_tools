6e225d549ae0dfd08f6e734f223238e15cc523ab
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2019-02-23 14:13:21 +0000

    refine expectation to be for invoices instead of sales_and_vat

--- reconcile_contracts_and_sales/spec/books_ingress_spec.rb
+++ reconcile_contracts_and_sales/spec/books_ingress_spec.rb
@@ -4,11 +4,7 @@ require_relative '../lib/books_ingress'
 describe 'BooksIngress' do
   it 'returns a hash of files when given an excel file location' do
     actual = BooksIngress.import_books
-    expected_keys = %i[
-      contracts
-      sales_and_vat
-      timesheets
-    ]
+    expected_keys = %i[contracts invoices timesheets]
     expect(actual.keys.sort).to eq(expected_keys.sort)
   end
 end

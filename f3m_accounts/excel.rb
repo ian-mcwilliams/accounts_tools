2f7fd1b73471c6e5ed3ab5557c73dc9eebd84d57
JohnMcWilliams <johnmcwilliams8@hotmail.com>
2019-02-25 20:03:17 +0000

    remove excel.rb

--- f3m_accounts/excel.rb
+++
@@ -1,9 +0,0 @@
-require 'simple_xlsx_reader'
-
-class Excel
-  attr_accessor :file_contents
-
-  def import_file_contents(file_path)
-    @file_contents = SimpleXlsxReader.open(file_path)
-  end
-end

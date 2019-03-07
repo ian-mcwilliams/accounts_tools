6c19606bdced6973f6bdf7465205d51c20d8f9f8
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2019-03-07 22:28:07 +0000

    add validate_log_hash method

---
+++ accounts_tools_helpers/lib/log_helpers.rb
@@ -0,0 +1,20 @@
+module LogHelpers
+
+  def self.validate_log_hash(log_hash)
+    raise("log_hash must be a Hash, got: #{log_hash.class}") unless log_hash.is_a?(Hash)
+    raise('log_hash must contain a :static_text key') unless log_hash.has_key?(:static_text)
+    unless log_hash[:static_text].is_a?(String)
+      raise("static_text must be a String, got: #{log_hash[:static_text].class}")
+    end
+    raise('static_text must not be empty') if log_hash[:static_text].empty?
+    if log_hash.has_key?(:wait_text) && !log_hash[:wait_text].is_a?(String)
+      raise("wait_text must be a String if provided, got: #{log_hash[:wait_text].class}")
+    end
+    raise('wait_text must not be empty') if log_hash.has_key?(:wait_text) && log_hash[:wait_text].empty?
+    if log_hash.has_key?(:done_text) && !log_hash[:done_text].is_a?(String)
+      raise("done_text must be a String if provided, got: #{log_hash[:done_text].class}")
+    end
+    raise('done_text must not be empty') if log_hash.has_key?(:done_text) && log_hash[:done_text].empty?
+  end
+
+end
